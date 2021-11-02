090f4a1cc06bae1648ef6aff29c4541261c13844
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2021-11-02 22:55:08 +0300

    creat an API key

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ class Config:
     '''
     General configuration parent class
     '''
-    pass
+ARTICLE_API_BASE_URL = 'https://newsapi.org/v2/top-headlines?{}business&apiKey={}'
 
 
 
---
+++ app/request.py
@@ -0,0 +1,4 @@
+from app import app
+ #Getting the API key
+
+api_key = app.config['ARTICE_API_KEY']
